591836a2680ccb44ff7bdb0aabc16b5b785be800
Thalles Marchetti <linkmarchetti@gmail.com>
2019-07-23 14:46:58 -0300

    update plugins

--- zshrc.sh
+++ zshrc.sh
@@ -8,11 +8,9 @@ sudo echo "ask for password before all the scripts"
 wget -O $HOME/.zshrc https://gist.githubusercontent.com/thallesmarchetti/bb4ed1f801a77fa068e6b738ca1b5b21/raw/e1c48f79d07a84fe823432613eec0af84cbeb74e/.zshrc
 
 # install oh-my-zsh plugins
-git clone https://github.com/djui/alias-tips.git $HOME/.oh-my-zsh/custom/plugins/alias-tips
-git clone https://github.com/supercrabtree/k $HOME/.oh-my-zsh/custom/plugins/k
-git clone https://github.com/rupa/z $HOME/.oh-my-zsh/custom/plugins/z
-git clone https://github.com/Valiev/almostontop.git $HOME/.oh-my-zsh/custom/plugins/almostontop
 
-git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+## syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+## autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
